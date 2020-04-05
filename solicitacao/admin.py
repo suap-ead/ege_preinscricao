ed833eeeee9c2f47729dbfc0eee099978bf8b0b9
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-04-05 19:29:45 -0300

    [ADD] cadastro da documentação

--- solicitacao/admin.py
+++ solicitacao/admin.py
@@ -1,6 +1,16 @@
 from django.contrib.admin import register, ModelAdmin, TabularInline
 # from import_export.admin import ImportExportModelAdmin
-from .models import Edital, Chamada, Selecionado, Solicitacao
+from .models import Documentacao, Edital, DocumentoExigido, Chamada, Selecionado, Solicitacao
+
+
+@register(Documentacao)
+class DocumentacaoAdmin(ModelAdmin):
+    list_display = ['identificacao']
+    search_fields = ['identificacao']
+
+
+class DocumentoExigidoInline(TabularInline):
+    model = DocumentoExigido
 
 
 @register(Edital)
@@ -8,13 +18,17 @@ class EditalAdmin(ModelAdmin):
     list_display = ['identificacao', 'titulo', 'pagina']
     search_fields = ['identificacao', 'titulo']
 
+    inlines = [DocumentoExigidoInline]
+
+
 class SelecionadoInline(TabularInline):
     model = Selecionado
+
 
 @register(Chamada)
 class ChamadaAdmin(ModelAdmin):
     list_display = ['chamada', 'edital', 'tipo_chamada', 'inicio_solicitacoes', 'fim_solicitacoes']
-    list_filter = ['tipo_chamada',  'edital__identificacao', 'chamada']
+    list_filter = ['tipo_chamada',  'edital__identificacao']
     search_fields = ['edital__identificacao', 'chamada']
 
     autocomplete_fields = ['edital']
