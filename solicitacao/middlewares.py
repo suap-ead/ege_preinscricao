c3be51c9b60862957747d86f3ff3051077b06b3d
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-04-05 21:46:34 -0300

    [FIX] dava erro quando não estava autenticado

--- solicitacao/middlewares.py
+++ solicitacao/middlewares.py
@@ -5,7 +5,6 @@ from django.contrib.auth import load_backend
 from django.contrib.auth.backends import RemoteUserBackend
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.deprecation import MiddlewareMixin
-from django.shortcuts import get_object_or_404
 from .models import Selecionado
 
 
@@ -13,4 +12,5 @@ class SelecionadoMiddleware(MiddlewareMixin):
     def process_request(self, request):
         selecionado_id = request.session.get("selecionado_id", None)
         if selecionado_id:
-            request.selecionado = get_object_or_404(Selecionado, id=selecionado_id)
+            request.selecionado = Selecionado.objects.filter(id=selecionado_id).first()
+
