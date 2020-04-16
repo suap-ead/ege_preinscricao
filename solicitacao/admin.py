fa9e26c7dc271d5008c80c0857323b348279ce4a
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2020-04-16 03:25:00 -0300

    [ADD] admin de solicitação/avaliação

--- solicitacao/admin.py
+++ solicitacao/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.admin import register, ModelAdmin, TabularInline
 from import_export.admin import ImportExportModelAdmin
-from .models import Documentacao, Edital, DocumentoExigido, Chamada, Selecionado, Solicitacao
+from .models import Documentacao, Edital, DocumentoExigido, Chamada, Selecionado, Solicitacao, Documento
 
 
 @register(Documentacao)
@@ -33,3 +33,77 @@ class ChamadaAdmin(ImportExportModelAdmin):
 
     autocomplete_fields = ['edital']
     inlines = [SelecionadoInline]
+
+
+
+class DocumentoInline(TabularInline):
+    model = Documento
+    exclude = ['sha512_arquivo']
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@register(Solicitacao)
+class SolicitacaoAdmin(ImportExportModelAdmin):
+    list_display = ['nome', 'selecionado']
+    list_filter = ['selecionado__chamada__tipo_chamada', 'selecionado__chamada__edital__identificacao']
+    search_fields = ['selecionado__chamada__chamada', 'selecionado__chamada__edital__identificacao']
+
+    inlines = [DocumentoInline]
+
+    readonly_fields = ['selecionado', 'data_conclusao_intercambio', 'linha_pesquisa', 
+                       'nome', 'nome_social', 'sexo', 'data_nascimento', 'estado_civil', 
+                       'nome_pai', 'estado_civil_pai', 'pai_falecido', 'nome_mae', 'estado_civil_mae',
+                       'mae_falecida', 'responsavel', 'email_responsavel', 'parentesco_responsavel', 'cpf_responsavel', 
+                       'cep', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'tipo_zona_residencial',
+                       'telefone_principal', 'telefone_secundario', 'telefone_adicional_1', 'telefone_adicional_2', 
+                       'email_pessoal', 'aluno_pne', 'tipo_necessidade_especial', 'tipo_transtorno', 'superdotacao', 
+                       'utiliza_transporte_escolar_publico', 'poder_publico_responsavel_transporte', 
+                       'tipo_veiculo', 'tipo_sanguineo', 'pais_origem', 'estado_naturalidade', 'naturalidade', 'raca', 
+                       'nivel_ensino_anterior', 
+                       'tipo_instituicao_origem', 
+                       'ano_conclusao_estudo_anterior', 
+                       'numero_rg', 
+                       'uf_emissao_rg', 
+                       'orgao_emissao_rg', 
+                       'data_emissao_rg', 
+                       'numero_titulo_eleitor', 
+                       'zona_titulo_eleitor', 
+                       'secao', 
+                       'data_emissao_titulo_eleitor', 
+                       'uf_emissao_titulo_eleitor', 
+                       'numero_carteira_reservista', 
+                       'regiao_carteira_reservista', 
+                       'serie_carteira_reservista', 
+                       'estado_emissao_carteira_reservista', 
+                       'nivel_ensino_anterior', 
+                       'ano_carteira_reservista', 
+                       'tipo_certidao', 
+                       'numero_certidao', 
+                       'folha_certidao', 
+                       'livro_certidao', 
+                       'data_emissao_certidao', 
+                       'matricula_certidao', 
+                       'autorizacao_carteira_estudantil', 
+                       'organizacao_didatica', 
+                       'autodeclaracao_etnia', 
+                       'penal', 
+                       'veracidade', 
+                       'confirmacao', 
+                       'enviada_em', 
+                       'sha512_solicitacao', 
+                       'iniciada_em', 
+                       'salvo_em', 
+                       ]
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
